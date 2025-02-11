e4f2f124ad8295cf2a2d92b416eb7d5108260bb1
vishesh1708 <svishesh1708@gmail.com>
2025-02-11 18:51:29 +0530

    added app to settings .

--- resume_analyzer/analyzer/models.py
+++ resume_analyzer/analyzer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    skills = models.TextField()
+    experience = models.TextField()
+    education = models.TextField()
+    resume = models.FileField(upload_to='resumes/')
+
+    def __str__(self):
+        return self.name
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    required_skills = models.TextField()
+
+    def __str__(self):
+        return self.title
